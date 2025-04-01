43142e41efaca4b90b74b39913f2b3bf0884a3b2
EmersonUrielVargas <emrvargaitaz@gmail.com>
2025-04-01 18:18:15 -0500

    add getter and setters client

--- src/main/java/com/platzi/market/persistance/entity/Cliente.java
+++ src/main/java/com/platzi/market/persistance/entity/Cliente.java
@@ -16,10 +16,58 @@ public class Cliente {
 
     private String apellidos;
 
-    private String celular;
+    private Integer celular;
 
     @Column(name = "correo_electronico")
     private String correoElectronico;
 
     private String direccion;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Integer getCelular() {
+        return celular;
+    }
+
+    public void setCelular(Integer celular) {
+        this.celular = celular;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
 }
